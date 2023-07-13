f77d0651ba139355c81b7a727d8f56157425c985
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-07-13 17:38:17 -0400

    add controller to the user route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,33 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const users = require('../data/users.json').users
+//const users = require('../data/users.json').users
+const { getUsers, getUser, createUser } = require('../controllers/users')
 
 
-router.get('/', (req, res) => {
-    console.log('/api/v1/users')
-    res.json(users)
-})
-
-router.get('/:id', (req, res) => {
-    console.log('/api/v1/users/:id')
-    const id = parseInt(req.params.id)
-    const user = users.find(user => user.id === id)
-    if (!user) {
-        return res.status(404).json({ error: 'User not found'})
-    } else {
-        res.json(user)
-    }
-})
-
-
-router.post('/', (req, res) => {
-    console.log('/api/v1/users')
-    const newUser = req.body
-    newUser.id = users.length + 1
-    users.push(newUser)
-    res.status(201).json(newUser)
-})
+router.route('/').get(getUsers).post(createUser)
+router.route('/:id').get(getUser)
 
 
 module.exports = router

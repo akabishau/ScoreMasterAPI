e53b113da2e26647db503c0849e89b22551e2539
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-07-14 15:14:22 -0400

    set up empty register and login routes

---
+++ src/controllers/auth.js
@@ -0,0 +1,12 @@
+const User = require('../models/User')
+
+const register = async (req, res) => {
+    console.log('register')
+}
+
+const login = async (req, res) => {
+    console.log('login')
+}
+
+
+module.exports = { register, login }

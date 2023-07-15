ae020b9c2109073a26deaed3490d3c0332ad23e8
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-07-15 14:25:36 -0400

    install dependencies and add basic dashboard route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const userRouter = require('./users')
 const authRouter = require('./auth')
+const dashboardRouter = require('./dashboard')
 
 
 router.use('/users', userRouter)
 router.use('/auth', authRouter)
+router.use('/dashboard', dashboardRouter)
 
 module.exports = router

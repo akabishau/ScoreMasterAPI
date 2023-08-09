2a73ae32cde1dfa2b8664c2a8e857765f04dba19
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-08-09 13:22:03 -0400

    refactor validation errors

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,17 +1,31 @@
 import { Request, Response, NextFunction } from 'express'
 import AppError from '../errors/AppError'
 
+const includeContext = process.env.NODE_ENV === 'development'
+
+function handleValidationErrors(errors: any) {
+  const validationErrors = Object.keys(errors.errors).map(key => ({
+    field: key,
+    message: errors.errors[key].message
+  }))
+
+  return {
+    status: 'error',
+    statusCode: AppError.BAD_REQUEST,
+    message: 'Validation error',
+    errors: validationErrors,
+    ...(includeContext && { context: errors })
+  }
+}
+
 export default function (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const includeContext = process.env.NODE_ENV === 'development'
-
   if (err instanceof AppError) {
     err.log()
-
     const { statusCode, message, context } = err
     const errorResponse = {
       status: 'error',
@@ -22,12 +36,19 @@ export default function (
     return res.status(statusCode).json(errorResponse)
   }
 
+  // Handle Mongoose validation errors
+  if (err.name === 'ValidationError') {
+    console.error('Validation Errors:', err)
+    const validationErrorResponse = handleValidationErrors(err)
+    return res.status(AppError.BAD_REQUEST).json(validationErrorResponse)
+  }
+
   console.error('Unhandled Error:', err)
   const defaultErrorResponse = {
     status: 'error',
-    statusCode: 500,
+    statusCode: AppError.INTERNAL,
     message: 'Internal Server Error',
     ...(includeContext && { context: err })
   }
-  res.status(500).json(defaultErrorResponse)
+  res.status(AppError.INTERNAL).json(defaultErrorResponse)
 }
